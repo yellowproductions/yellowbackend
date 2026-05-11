326b28d107cce5cc443bde1b30f53dd5d3c4ff62
yellowproductions <creative@yellowproductions.co.in>
2026-05-11 11:39:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ async function getDropboxToken() {
 function setCORS(res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 }
 
 // Ensure a Dropbox folder exists. Silently ignores "already exists" errors
